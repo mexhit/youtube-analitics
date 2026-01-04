7adf97cf6fc0366b2423cb32dd35a546d6a812c8
mexhit <mexhii.asllani@gmail.com>
2026-01-04 12:57:57 +0100

    Remove `generateStaticParams` function from `live` page to simplify code structure.

--- src/app/dashboard/[channelId]/live/[live]/page.tsx
+++ src/app/dashboard/[channelId]/live/[live]/page.tsx
@@ -1,25 +1,6 @@
 import { Suspense } from "react";
 import { DataPage } from "@/app/dashboard/[channelId]/live/[live]/DataPage";
 import { Loader } from "@/app/dashboard/[channelId]/live/[live]/loader";
-import { Channels } from "@/app/dashboard/[channelId]/live/[live]/constants";
-
-export async function generateStaticParams() {
-  const staticParams = [];
-
-  for (const key in Channels) {
-    staticParams.push({
-      channelId: Channels[key].id,
-      live: "true",
-    });
-
-    staticParams.push({
-      channelId: Channels[key].id,
-      live: "false",
-    });
-  }
-
-  return staticParams;
-}
 
 export default async function Page({
   params,
